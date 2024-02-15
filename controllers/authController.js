0fad5fbac2f81105e11e31d2388793a46a03e75f
AbdallahSalah <abdallahsalahsalem9@gmail.com>
2024-02-16 00:02:41 +0200

    Add auth/restrictTo based on user role

--- controllers/authController.js
+++ controllers/authController.js
@@ -103,3 +103,15 @@ exports.protect = catchAsyncError(async (req, res, next) => {
   req.user = currentUser;
   next();
 });
+
+exports.restrictTo = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({
+        status: 'fail',
+        message: 'You dont have the permission for this action',
+      });
+    }
+    next();
+  };
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,6 +6,9 @@ const router = express.Router();
 router.route('/signup').post(authController.signup);
 router.route('/login').post(authController.login);
 
+router.use(authController.protect);
+router.use(authController.restrictTo('admin'));
+
 router
   .route('/')
   .get(usersController.getAllUsers)
