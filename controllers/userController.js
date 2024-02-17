a239885fe61963d93a0292a024d5799f93debc1a
AbdallahSalah <abdallahsalahsalem9@gmail.com>
2024-02-17 22:08:17 +0200

    Refactor userController using handlerFactory

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,34 +1,13 @@
 const User = require('./../models/userModel');
 const catchAsyncError = require('./../utils/catchAsyncError');
+const factory = require('./handlerFactory');
 
-exports.getAllUsers = catchAsyncError(async (req, res) => {
-  const users = await User.find();
-  if (!users) {
-    res.status(404).json({
-      status: 'fail',
-      data: {},
-    });
-  } else {
-    res.status(200).json({
-      status: 'success',
-      data: { users },
-    });
-  }
-});
-exports.addUser = catchAsyncError(async (req, res) => {
-  const user = await User.create(req.body);
-  if (!user) {
-    res.status(404).json({
-      status: 'fail',
-      data: {},
-    });
-  } else {
-    res.status(200).json({
-      status: 'success',
-      data: { user },
-    });
-  }
-});
+exports.getAllUsers = factory.getAll(User);
+
+exports.updateUser = factory.updateOne(User);
+
+exports.deleteUser = factory.deleteOne(User);
+
 exports.getUser = catchAsyncError(async (req, res) => {
   const user = await User.findById(req.params.id);
   if (!user) {
@@ -40,38 +19,6 @@ exports.getUser = catchAsyncError(async (req, res) => {
     res.status(200).json({
       status: 'success',
       data: { user },
-    });
-  }
-});
-exports.updateUser = catchAsyncError(async (req, res) => {
-  const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-    runValidators: true,
-  });
-
-  if (!user) {
-    res.status(404).json({
-      status: 'fail',
-      data: {},
-    });
-  } else {
-    res.status(200).json({
-      status: 'success',
-      data: { user },
-    });
-  }
-});
-exports.deleteUser = catchAsyncError(async (req, res) => {
-  const user = await User.findByIdDelete(req.params.id);
-  if (!user) {
-    res.status(404).json({
-      status: 'fail',
-      data: {},
-    });
-  } else {
-    res.status(204).json({
-      status: 'success',
-      data: null,
     });
   }
 });
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,15 +6,12 @@ const router = express.Router();
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 router.post('/forgotPassword', authController.forgotPassword);
-router.patch('/resetPassword', authController.resetPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
 
 router.use(authController.protect);
 // router.use(authController.restrictTo('admin'));
 
-router
-  .route('/')
-  .get(usersController.getAllUsers)
-  .post(usersController.addUser);
+router.route('/').get(usersController.getAllUsers);
 
 router
   .route('/:id')
