3c3978125706c253243583344f7533f426ab616e
AbdallahSalah <abdallahsalahsalem9@gmail.com>
2024-02-17 19:07:58 +0200

    Refactor error handling in task & activity controllers

--- controllers/activityController.js
+++ controllers/activityController.js
@@ -2,9 +2,15 @@ const Activity = require('./../models/activityModel');
 const User = require('./../models/userModel');
 const catchAsyncError = require('./../utils/catchAsyncError');
 const factory = require('./handlerFactory');
+const AppError = require('./../utils/appError');
 
 exports.setActivity = catchAsyncError(async (req, res, next) => {
   const contributer = await User.findOne({ email: req.body.contributer });
+  if (!contributer) {
+    return next(
+      new AppError('Activity contributer in not registered on system', 404),
+    );
+  }
   req.body.taskID = req.params.taskId;
   req.body.contributerID = contributer._id;
   next();
--- controllers/taskController.js
+++ controllers/taskController.js
@@ -3,23 +3,43 @@ const Project = require('./../models/projectModel');
 const User = require('./../models/userModel');
 const catchAsyncError = require('./../utils/catchAsyncError');
 const factory = require('./handlerFactory');
+const AppError = require('./../utils/appError');
 
-exports.setTask = catchAsyncError(async (req, res, next) => {
+const setModerator = async (req, next) => {
   const moderator = await User.findOne({ email: req.body.moderatorEmail });
+  if (!moderator) {
+    return next(
+      new AppError('Task moderator is not registered on system', 404),
+    );
+  }
+  return moderator;
+};
+const setContributers = async (req, next) => {
   let contributers = [];
   req.body.contributers.forEach((el) => {
     contributers.push(User.findOne({ email: el }));
   });
-  contributers = (await Promise.all(contributers)).map((el) => el._id);
+  contributers = (await Promise.all(contributers)).map((el) => {
+    if (!el) {
+      return next(
+        new AppError("Task's contributers are not registered on system", 404),
+      );
+    }
+    return el._id;
+  });
+  return contributers;
+};
+exports.setTask = catchAsyncError(async (req, res, next) => {
+  const moderator = await setModerator(req, next);
+  const contributers = await setContributers(req, next);
+
   const task = {
+    ...req.body,
     projectID: req.params.projectID,
     moderatorID: moderator._id,
-    name: req.body.name,
     contributers: contributers,
-    description: req.body.description,
-    startDate: req.body.startDate,
-    endDate: req.body.endDate,
   };
+
   req.body = task;
   next();
 });
