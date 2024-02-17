b6926c0fae1843a0a2b0678d03dc8239b82d5b2f
AbdallahSalah <abdallahsalahsalem9@gmail.com>
2024-02-17 21:57:51 +0200

    Add forgot/reset password routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,8 +3,10 @@ const usersController = require('./../controllers/userController');
 const authController = require('./../controllers/authController');
 const router = express.Router();
 
-router.route('/signup').post(authController.signup);
-router.route('/login').post(authController.login);
+router.post('/signup', authController.signup);
+router.post('/login', authController.login);
+router.post('/forgotPassword', authController.forgotPassword);
+router.patch('/resetPassword', authController.resetPassword);
 
 router.use(authController.protect);
 // router.use(authController.restrictTo('admin'));
