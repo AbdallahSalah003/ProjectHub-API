e6b65038bb5de58135fcf4d4872c964663e48602
AbdallahSalah <abdallahsalahsalem9@gmail.com>
2024-02-17 16:53:47 +0200

    Handel DB errors in GlobalErrorHandeler for Production

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -9,6 +9,38 @@ const developmentErrorRespose = (res, err) => {
   });
 };
 
+const productionErrorResponse = (res, err) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.error('ERROR !!!', err);
+    // send generic message to the client
+    res.status(500).json({
+      status: 'fail',
+      message: 'Something went wrong, please try again later!',
+    });
+  }
+};
+
+const handelCastErrorsDB = (error) => {
+  return new AppError(`Invalid ${error.path}: ${error.value}`, 400);
+};
+const handelDuplicateErrorsDB = (error) => {
+  return new AppError(
+    `Duplicate Key on '${error.keyValue.name}', Please use another value!`,
+    400,
+  );
+};
+const handelValidationErrorDB = (error) => {
+  let message = Object.values(error.errors)
+    .map((err) => err.message)
+    .join('.  ');
+  return new AppError(message, 400);
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
@@ -16,10 +48,13 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     developmentErrorRespose(res, err);
   } else {
-    //Error In Production needs to be handeled
-    res.status(err.statusCode).json({
-      status: err.status,
-      message: 'An error occured!',
-    });
+    let error = { ...err, name: err.name };
+    if (error.name === 'CastError') e;
+    rror = handelCastErrorsDB(error);
+    if (error.code == 11000) error = handelDuplicateErrorsDB(error);
+    if (error.name === 'ValidationError')
+      error = handelValidationErrorDB(error);
+
+    productionErrorResponse(res, error);
   }
 };
