9d907a3a49902078b0ebfa10f13f649f63c0afdf
AbdallahSalah <abdallahsalahsalem9@gmail.com>
2024-02-18 17:24:57 +0200

    Limit requests from same IP

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 const userRouter = require('./routes/userRoutes');
 const projecRouter = require('./routes/projectRoutes');
 const taskRouter = require('./routes/taskRoutes');
@@ -15,6 +16,13 @@ app.use(express.json({ limit: '10kb' }));
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const limitter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('./api', limitter);
 
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/projects', projecRouter);
